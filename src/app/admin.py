5161425844f0e1a499e550868b8414dba44d6f21
Madelaine Colicheo <mrcolicheom@gmail.com>
2024-12-27 18:03:13 -0400

    Registrando modelos

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Categoria, Producto, Ingrediente, ProductoIngrediente, Cliente, Pedido, DetallePedido
 
-# Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Producto)
+admin.site.register(Ingrediente)
+admin.site.register(ProductoIngrediente)
+admin.site.register(Cliente)
+admin.site.register(Pedido)
+admin.site.register(DetallePedido)
